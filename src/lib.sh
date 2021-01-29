7fbdac1708432f8fe83cb413391b18385f57fd1b
ytyaru <ytyaru@outlook.jp>
2021-01-29 13:41:18 +0900

    jtalk

--- src/lib.sh
+++ src/lib.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
 OPENJTALK_VOICE_DIR="$HOME/root/sys/env/tool/openjtalk/voice/"
+OPENJTALK_DEFAULT_VOICE="$HOME/root/sys/env/tool/openjtalk/voice/MMDAgent_Example-1.8/Voice/mei/mei_normal.htsvoice"
 GetVoices() { # $1: PATH_DIR(option)
 	local PATH_DEFAULT="${1:-$OPENJTALK_VOICE_DIR}"
 	local PATH_DIR="${1:-$PATH_DEFAULT}"
 	echo "$(find "${PATH_DIR}" -name *.htsvoice)"
+}
+GetDefaultVoice() { # $1: PATH_DIR(option)
+	local PATH_DEFAULT="${1:-$OPENJTALK_VOICE_DIR}"
+	local names=(
+		"mei_normal.htsvoice"	# ---------1
+		"tohoku-f01-neutral.htsvoice"
+		"takumi_normal.htsvoice"
+		"type-beta.htsvoice"
+		"蒼歌ネロ.htsvoice"
+		"ワタシ.htsvoice"
+		"緋惺.htsvoice"
+		"天月りよん.htsvoice"
+		"nitech_jp_atr503_m001.htsvoice"
+		"白狐舞.htsvoice"		# ---------2
+		"なないろニジ.htsvoice"
+		"空唄カナタ.htsvoice"
+		"薪宮風季.htsvoice" 
+		"遊音一莉.htsvoice"		# ---------3
+		"唱地ヨエ.htsvoice"
+		"句音コノ。.htsvoice"	# ---------4
+		"月音ラミ_1.0.htsvoice"
+	)
+	for name in "${names[@]}"; do
+		path="$(find "$(cd "${PATH_DEFAULT}"; pwd)" -name "${name}")"
+		[ -f "${path}" ] && { echo "${path}"; return; }
+	done
 }
 GetRandomVoice() { # $1: PATH_DIR(option)
 	local PATHS="$(GetVoices "$1")"
@@ -22,4 +49,8 @@ OpenJTalk() { # $1: message(option), $2: voice(option), $3: output:(option:/dev/
 		| aplay
 	error=$?; [ $error -ne 0 ] && { echo "ERROR: OpenJTalk $MESSAGE\n$VOICE\n$OUTPUT"; }
 }
-
+jtalk() {
+	local MESSAGE="${1:-こんにちは。これはテスト用メッセージです。}"
+	local VOICE="$(GetDefaultVoice)"
+	OpenJTalk "$MESSAGE" "$VOICE"
+}
