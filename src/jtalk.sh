ba4c0f73568a7e1dce4bbc1eabf28960c403b951
ytyaru <ytyaru@outlook.jp>
2021-01-29 15:56:44 +0900

    GetDefaultDic

--- src/jtalk.sh
+++ src/jtalk.sh
@@ -35,6 +35,16 @@ GetDefaultVoice() { # $1: PATH_DIR(option)
 		[ -f "${path}" ] && { echo "${path}"; return; }
 	done
 }
+SearchVoice() {
+	[ -f "$1" ] && { echo "$1"; return; }
+	local paths=()
+#	for arg in "$@"; do { paths+=("$arg"); } done
+	paths+=("$OPENJTALK_VOICE_DIR")
+	paths+=("/usr/share/hts-voice/")
+	for path in "${paths[@]}"; do
+		[ -d "$path" ] && echo "$(find "$(cd "${path}"; pwd)" -name "$1.htsvoice")"
+	done
+}
 GetRandomVoice() { # $1: PATH_DIR(option)
 	local PATHS="$(GetVoices "$1")"
 	local COUNT=$(echo "${PATHS}" | wc -l)
@@ -45,8 +55,8 @@ GetDefaultDic() {
 	local paths=(
 		"/var/lib/mecab/dic/open-jtalk/naist-jdic"
 	)
-	for path in "${names[@]}"; do
-		[ -f "${path}" ] && { echo "${path}"; return; }
+	for path in "${paths[@]}"; do
+		[ -d "${path}" ] && { echo "${path}"; return; }
 	done
 	echo '/usr/local/dic'
 }
@@ -60,33 +70,43 @@ OpenJTalk() { # $1: message(option), $2: voice(option), $3: output:(option:/dev/
 	local cmd_aplay=" | aplay ${cmd_aplay_file}"
 	local cmd="echo \"$MESSAGE\" | open_jtalk -x \"$DIC\" -m \"$VOICE\" -ow \"$OUTPUT\" $cmd_aplay"
 	eval "${cmd}"
-#	echo "$cmd_aplay"
-#	echo "$MESSAGE" | open_jtalk \
-#		-x /usr/local/dic \
-#		-m "$VOICE" \
-#		-ow "${OUTPUT}" \
-#		"${cmd_aplay}"
-##		| aplay
 	error=$?; [ $error -ne 0 ] && { echo "ERROR: OpenJTalk $MESSAGE\n$VOICE\n$OUTPUT"; }
 }
-jtalk() {
-#	local MESSAGE="${1:-こんにちは。これはテスト用メッセージです。}"
-#	local VOICE="$(GetDefaultVoice)"
-#	OpenJTalk "$MESSAGE" "$VOICE"
+Run() { # jtalk
+	Help() {
+		local text="$(cat <<-EOS
+			音声合成する。日本語。OpenJTalkを使う。
+			Usage:
+			  jtalk MESSAGE [VOICE]
+			Parameters:
+			  MESSAGE  発話させたいテキスト
+			  VOICE    発話させたい.htsvoiceファイルパス
+			env:
+			  OPENJTALK_VOICE_DIR  .htsvoiceがあるルートディレクトリ
+			    "${OPENJTALK_VOICE_DIR}"
+			examples:
+			  jtalk これを読み上げます
+			  jtalk -r 声をランダムにします
+			  jtalk -v type-beta 初音ミクです
+			  jtalk -o /tmp/work/a.wav 音声ファイルに録音します
+			this: "$(realpath "${BASH_SOURCE:-0}")"
+		EOS
+		)"
+		echo "$text"
+	}
 	local OPT_IS_RANDOM_VOICE=0
 	local OPT_SELECTED_VOICE="$(GetDefaultVoice)"
 	local OPT_SELECTED_DIC_PATH="$(GetDefaultDic)"
 	local OPT_OUTPUT_FILE_PATH="/dev/stdout"
 	local OPT_SPEED="1.0" # -r 1.0
-	local OPT_VOLUME="1.0" # -g 0.0
-	while getopts ':ro:d:h' OPT; do
+	local OPT_VOLUME="0.0" # -g 0.0
+	while getopts ':rv:o:d:h' OPT; do
 		case $OPT in
-#		r) OPT_SELECTED_VOICE="$(GetRandomVoice)" ;;
-		r) echo 'らんだむったー' ;;
-		v) OPT_SELECTED_VOICE="$OPTARG" ;;
+		r) OPT_SELECTED_VOICE="$(GetRandomVoice)" ;;
+		v) OPT_SELECTED_VOICE="$(SearchVoice "$OPTARG")" ;;
 		d) OPT_SELECTED_DIC_PATH="$OPTARG" ;;
 		o) OPT_OUTPUT_FILE_PATH="$OPTARG" ;;
-		h) echo  "Help";;
+		h) Help; return;;
 		:) echo  "[ERROR] Option argument is undefined.";;   # 
 		\?) echo "[ERROR] Undefined options.";;
 		esac
@@ -95,10 +115,7 @@ jtalk() {
 	echo "VOICE: ${OPT_SELECTED_VOICE}"
 	echo "DIC:   ${OPT_SELECTED_DIC_PATH}"
 	echo "OUT:   ${OPT_OUTPUT_FILE_PATH}"
-#	echo "${}"
-#	echo "${}"
 	local MESSAGE="${1:-こんにちは。これはテスト用メッセージです。}"
-	echo "${MESSAGE}"
 	echo "MSG:   ${MESSAGE}"
 	OpenJTalkCmd() {
 		local VOICE="$OPT_SELECTED_VOICE"
@@ -107,18 +124,16 @@ jtalk() {
 		echo "open_jtalk -x \"$DIC\" -m \"$VOICE\" -ow \"$OUTPUT\" -r $OPT_SPEED -g $OPT_VOLUME "
 	}
 	AplayCmd() {
+		[ "$OUTPUT" = '/dev/stdout' ] && { echo ' | aplay'; return; }
 		local OUTPUT="$OPT_OUTPUT_FILE_PATH"
 		local cmd_aplay_file=""
 		[ "$OUTPUT" != '/dev/stdout' ] && cmd_aplay_file="\"${OUTPUT}\""
-		local cmd_aplay="aplay ${cmd_aplay_file} "
+		local cmd_aplay=" ; aplay ${cmd_aplay_file}; "
 		echo "$cmd_aplay"
 	}
-	local cmd="echo \"$MESSAGE\" | $(OpenJTalkCmd) | $(AplayCmd)"
+	local cmd="echo \"$MESSAGE\" | $(OpenJTalkCmd) $(AplayCmd)"
 	echo "${cmd}"
 	eval "${cmd}"
-#	OpenJTalk "$MESSAGE" "$OPT_SELECTED_VOICE" "$OPT_OUTPUT_FILE_PATH"
 }
-jtalk -h
-#jtalk -r 'こんにちは'
-#jtalk -r -o /tmp/a.wav 'こんにちは'
-#jtalk -v 'voice.v' -d 'a.dic' -o 'a.wav' 'こんにちは'
+Run "$@"
+
